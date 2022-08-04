9ea8342c2478dd4c01dc644a7baa3eb0a40ae2fe
Quagge1 <108851187+Quagge1@users.noreply.github.com>
2022-08-03 23:01:35 -0700

    Activity11

---
+++ Activity11/Dice.cs
@@ -0,0 +1,51 @@
+﻿public class Dice
+{
+	//variables
+	private int sides;                      
+	private int value;
+	//randome class call
+	Random rnd = new Random();
+	
+
+	//defult consturcor
+	public Dice()
+	{                           	
+		sides = 6;
+		value = 1;
+	}
+	//Overloaded constructor
+	public Dice(int numSides)
+	{               
+		sides = numSides;
+		value = 1;
+	}
+	//getters and setters below
+	public int getSides()
+	{                   
+		return sides;
+	}
+	public void setSides(int sides)
+	{       
+		this.sides = sides;
+	}
+	public int getValue()
+	{
+		return value;
+	}
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+	//Method to roll the dice
+	public void roll()
+	{
+		value = (int)rnd.Next(7) * sides + 1;   
+	}
+	public String toString()
+	{
+		return "(" + value + ")";               
+	}
+
+}
+
+
---
+++ Activity11/Driver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity11
+{
+    internal class Driver
+    {
+        public static void Main(String[] args)
+        {
+            //creating dice
+            Dice die1 = new Dice();
+            Dice die2 = new Dice();
+
+            
+            //variable for number of rolls
+            int rollCount;
+
+            rollCount = 0;
+
+            //do while loop to roll the dice unless they are both 1 or snake eyes
+            do
+            {
+                die1.roll();
+                die2.roll();
+                rollCount++;
+
+            } while ((die1.getValue() != 1) || (die2.getValue() != 1));
+
+
+
+            Console.WriteLine("It took " + rollCount + " rolls for you to roll snake eyes.");
+        }
+
+    }
+}
+
