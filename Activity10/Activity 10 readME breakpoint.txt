5c640931cd6a2bf7b199da6419958f87af9869c7
Quagge1 <108851187+Quagge1@users.noreply.github.com>
2022-08-07 16:20:01 -0700

    Activity10

---
+++ Activity10/Activity 10 readME breakpoint.txt
@@ -0,0 +1,4 @@
+Screen shot of break point is to illistrate where the program was breaking when run with provided code.
+Based off of that breakpoint is where I analysed the code. Once I changed it to get it to run. The output
+was showing the Calue of A being depicted as the union. That is where the idea of using a copy Set() to 
+hold the unionized data came from. 
---
+++ Activity10/Set.cs
@@ -0,0 +1,80 @@
+﻿//Corrected by <Patrick Quagge>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_10
+{
+    class Set
+    {
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+        //creating copy constructor to hold copied values
+        public Set(Set other)
+        {
+           this.elements = new List<int>();
+            for (int i = 0; i < other.elements.Count; i++)
+            {
+                elements.Add(other.elements[i]);
+            };
+        }
+
+        public bool addElement(int val)
+         {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+        private bool containsElement(int val)
+        {
+            for(int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            foreach(int i in elements)
+            {
+                str += i + "";
+            }
+            return str;
+        }
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+        public Set union(Set rhs)
+        {
+            //the way this method was constructed it was holding the data for A and returning that instead of the new unionized set
+            //by using a copy of the original object instead of the object itself we avoid making changes to it over the course of the method
+            
+            //create a new set to return
+            Set finalSet = new Set(this);
+            
+            for(int i = 0; i < rhs.elements.Count; i++)
+            {
+                finalSet.addElement(rhs.elements[i]);
+            }
+            //returning new set that is union only
+            //return rhs;
+            return finalSet;
+        }
+
+        
+    }
+}
